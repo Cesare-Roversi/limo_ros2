012db7faa1087088bc9052493737281828ebcbfa
Tommaso Faraci <tommaso.faraci3@gmail.com>
2026-04-01 18:20:35 +0200

    Only odometry missing

--- limo_car/launch/ackermann.launch.py
+++ limo_car/launch/ackermann.launch.py
@@ -7,10 +7,12 @@
 
 import os
 
+import launch
+
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch.actions import DeclareLaunchArgument
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
@@ -56,6 +58,12 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
 
 
     return LaunchDescription([
@@ -65,4 +73,5 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
         node_robot_state_publisher, 
+        joint_state_publisher_node
     ])
